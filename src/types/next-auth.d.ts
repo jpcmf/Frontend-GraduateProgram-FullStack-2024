e3077c83aadb554ea73129b5d9761ea97befa6a2
João Paulo Fricks <jpfricks@gmail.com>
2024-03-26 19:54:06 -0300

    feat: Create the header component

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,26 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    id?: any & DefaultSession["session"];
+    jwt?: any & DefaultSession["session"];
+    user: DefaultSession["user"] & {
+      id?: string;
+      username?: string;
+      about?: string;
+    };
+  }
+
+  interface User {
+    id?: string;
+    jwt?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    jwt?: string;
+  }
+}
