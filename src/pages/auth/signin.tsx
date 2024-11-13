7c1d345d7b5d6c370dc6728eeb75e962862fea8c
João Paulo Fricks <jpfricks@gmail.com>
2024-11-13 15:21:50 -0300

    feat: Add reCAPTCHA verification to sign-in process and update AuthContext

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
+import ReCAPTCHA from "react-google-recaptcha";
 import { z } from "zod";
 import { useRouter } from "next/router";
-import { useContext } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext, useRef, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Box, Button, Flex, Stack, Text, Link as ChakraLink } from "@chakra-ui/react";
 
@@ -12,63 +13,80 @@ import { Toast } from "@/components/Toast";
 import { AuthContext } from "@/contexts/AuthContext";
 import { LogoSkateHub } from "@/components/LogoSkateHub";
 
-const signInFormSchema2 = z.object({
+const signInFormSchema = z.object({
   email: z.string().email({ message: "E-mail deve ser um e-mail válido." }).min(1, { message: "Campo obrigatório." }),
   password: z.string().min(1, { message: "Campo obrigatório." })
 });
 
-type SignInFormSchema = z.infer<typeof signInFormSchema2>;
+type SignInFormSchema = z.infer<typeof signInFormSchema>;
 
 export default function SignIn() {
   const router = useRouter();
   const { signIn } = useContext(AuthContext);
   const { addToast } = Toast();
+  const [isVerified, setIsVerified] = useState(false);
+  const [isVerifiedError, setIsVerifiedError] = useState(false);
+  const recaptchaRef = useRef<ReCAPTCHA | null>(null);
 
   const {
     handleSubmit,
     register,
     formState: { errors, isSubmitting }
   } = useForm<SignInFormSchema>({
-    resolver: zodResolver(signInFormSchema2),
+    resolver: zodResolver(signInFormSchema),
     defaultValues: { email: "", password: "" },
     mode: "onChange"
   });
 
   const handleSignIn: SubmitHandler<SignInFormSchema> = async values => {
-    await signIn(values)
-      .then(_ => {})
-      .catch(error => {
-        console.log("error...", error);
+    if (isVerified) {
+      const recaptchaValue = recaptchaRef.current?.getValue();
+      const newValues = { ...values, recaptcha: recaptchaValue || undefined };
 
-        if (!error.response.data.error?.message) {
-          console.log("ping");
-        }
+      await signIn(newValues)
+        .then(_ => {})
+        .catch(error => {
+          recaptchaRef.current?.reset();
+          setIsVerified(false);
 
-        switch (error.response.data.error?.message) {
-          case "Your account email is not confirmed":
-            addToast({
-              title: "Erro de autenticação.",
-              message: "Confirme seu e-mail para acessar a plataforma.",
-              type: "warning"
-            });
-            break;
-          case "Your account has been blocked by an administrator":
-            addToast({
-              title: "Erro de autenticação.",
-              message:
-                "Sua conta está temporariamente bloqueada. Se você acabou de se cadastrar, por favor, aguarde enquanto suas informações estão sendo revisadas por nossa equipe.",
-              type: "error"
-            });
-            break;
-          default:
-            addToast({
-              title: "Erro de autenticação.",
-              message: "Verifique seus dados de login e tente novamente.",
-              type: "error"
-            });
-            break;
-        }
-      });
+          switch (error.response.data.error?.message) {
+            case "Your account email is not confirmed":
+              addToast({
+                title: "Erro de autenticação.",
+                message: "Confirme seu e-mail para acessar a plataforma.",
+                type: "warning"
+              });
+              break;
+            case "Your account has been blocked by an administrator":
+              addToast({
+                title: "Erro de autenticação.",
+                message:
+                  "Sua conta está temporariamente bloqueada. Se você acabou de se cadastrar, por favor, aguarde enquanto suas informações estão sendo revisadas por nossa equipe.",
+                type: "error"
+              });
+              break;
+            default:
+              addToast({
+                title: "Erro de autenticação.",
+                message: "Verifique seus dados de login e tente novamente.",
+                type: "error"
+              });
+              break;
+          }
+        });
+    } else {
+      setIsVerifiedError(true);
+      console.log("Please verify the reCAPTCHA.");
+    }
+  };
+
+  const onVerify = (token: string | null) => {
+    if (!token) return;
+
+    if (token) {
+      setIsVerified(true);
+      setIsVerifiedError(false);
+    }
   };
 
   return (
@@ -122,6 +140,23 @@ export default function SignIn() {
                   .
                 </Text>
               </Box>
+            </Flex>
+
+            <Flex flexDir="column">
+              <ReCAPTCHA
+                ref={recaptchaRef}
+                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
+                onChange={onVerify}
+                size="normal"
+                hl="pt-BR"
+                badge="inline"
+                id="recaptcha"
+              />
+              {isVerifiedError && (
+                <Text fontSize={"13.3px"} fontWeight="semibold" color="red.500" mt="1.5">
+                  Please verify that you are not a robot.
+                </Text>
+              )}
             </Flex>
           </Stack>
           <Button type="submit" mt="6" colorScheme="green" fontWeight="bold" size="lg" isLoading={isSubmitting}>
