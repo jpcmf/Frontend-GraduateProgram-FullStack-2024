35c9df4eb758b3cac77bd7b121d58a172eeabc38
João Paulo Fricks <jpfricks@gmail.com>
2024-12-07 12:14:58 -0300

    feat: Update user edit functionality

--- src/features/user/edit/index.tsx
+++ src/features/user/edit/index.tsx
@@ -1,49 +1,65 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Icon,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  Checkbox,
-  Text,
-  useBreakpointValue,
-  Spinner,
-  Link as ChakraLink,
-  Divider,
-  SimpleGrid,
-  VStack,
-  HStack
-} from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Divider, SimpleGrid, VStack, HStack } from "@chakra-ui/react";
 
-import { Input } from "@/components/Form/Input";
+import { Input } from "@/shared/components/Form/Input";
+import { Toast } from "@/components/Toast";
 import { Layout } from "@/shared/components/Layout";
+import { Textarea } from "@/shared/components/Form/Textarea";
+import { AuthContext } from "@/contexts/AuthContext";
 
 type RegisterForm = {
   name: string;
   email: string;
-  password: string;
-  password_confirmation: string;
+  about: string;
+  website_url: string;
+  // password: string;
+  // password_confirmation: string;
 };
 
 export function UserEdit() {
   const router = useRouter();
+  const { addToast } = Toast();
+
+  const { user, updateUser } = useContext(AuthContext);
+
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting }
+    formState: { errors, isSubmitting },
+    reset
   } = useForm<RegisterForm>();
 
+  useEffect(() => {
+    if (user) {
+      reset(user);
+    }
+  }, [user, reset]);
+
   const handleEditUser: SubmitHandler<RegisterForm> = async values => {
-    console.log("Editando...");
+    try {
+      await updateUser({
+        id: user ? user.id : "",
+        name: values.name,
+        email: values.email,
+        about: values.about,
+        website_url: values.website_url
+      });
+
+      addToast({
+        title: "Usuário editado com sucesso.",
+        message: "Seu perfil foi atualizado.",
+        type: "success"
+      });
+    } catch (error) {
+      addToast({
+        title: "Erro ao editar usuário.",
+        message: `Ocorreu um erro ao editar seu perfil: ${error}`,
+        type: "error"
+      });
+      console.log("Erro ao editar usuário:", error);
+    }
   };
 
   return (
@@ -54,12 +70,34 @@ export function UserEdit() {
             Editar
           </Heading>
           <Divider my="6" borderColor="gray.700" />
-          <VStack spacing={["6", "8"]}>
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input label="Nome completo" {...register("name")} error={errors.name} />
+          <VStack spacing="4">
+            <SimpleGrid minChildWidth="240px" spacing="4" w="100%">
+              <Input label="Nome completo" {...register("name")} error={errors.name} isDisabled />
               <Input type="email" label="E-mail" {...register("email")} error={errors.email} />
             </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+            <SimpleGrid minChildWidth="240px" spacing="4" w="100%">
+              <Flex flexDirection="column">
+                <Textarea label="Sobre você" placeholder="Sobre você..." {...register("about")} error={errors.about} />
+              </Flex>
+            </SimpleGrid>
+            <SimpleGrid minChildWidth="240px" spacing="4" w="100%">
+              <Input
+                isInputGroup
+                InputLeftAddonText="instagram.com/"
+                label="Perfil Instagram"
+                placeholder="Ex. nome_do_usuário"
+              />
+              <Input
+                isInputGroup
+                InputLeftAddonText="https://"
+                label="Website Pessoal"
+                placeholder="Ex. www.site.com.br"
+                {...register("website_url")}
+                error={errors.website_url}
+              />
+            </SimpleGrid>
+
+            {/* <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input type="password" label="Senha" {...register("password")} error={errors.password} />
               <Input
                 type="password"
@@ -67,14 +105,21 @@ export function UserEdit() {
                 {...register("password_confirmation")}
                 error={errors.password_confirmation}
               />
-            </SimpleGrid>
+            </SimpleGrid> */}
           </VStack>
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button as="a" size="sm" fontSize="sm" colorScheme="whiteAlpha" onClick={() => router.push("/dashboard")}>
+              <Button as="a" size="lg" fontSize="sm" colorScheme="whiteAlpha" onClick={() => router.push("/dashboard")}>
                 Cancelar
               </Button>
-              <Button type="submit" isLoading={isSubmitting} size="sm" fontSize="sm" colorScheme="blue">
+              <Button
+                type="submit"
+                isLoading={isSubmitting}
+                size="lg"
+                fontSize="sm"
+                colorScheme="green"
+                isDisabled={!user?.email}
+              >
                 Salvar
               </Button>
             </HStack>
