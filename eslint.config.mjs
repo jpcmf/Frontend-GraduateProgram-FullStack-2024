af79cef709a8a7ea8e9beaa6e02492eac7c30c62
João Paulo Fricks <jpfricks@gmail.com>
2025-10-07 00:31:56 -0300

    fix: update ESLint configuration imports and add new dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
