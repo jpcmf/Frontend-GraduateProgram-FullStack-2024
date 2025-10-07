4daf7e365417d17ceb96d71ab80a81342bffde3a
João Paulo Fricks <jpfricks@gmail.com>
2025-10-07 01:07:07 -0300

    fix: reorder imports

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,6 +1,6 @@
+import { cloneElement, ReactElement } from "react";
 import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/router";
-import { cloneElement, ReactElement } from "react";
 
 interface ActiveLinkProps extends LinkProps {
   children: ReactElement;
