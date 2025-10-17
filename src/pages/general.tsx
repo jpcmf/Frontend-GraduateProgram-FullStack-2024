79d4221e5c1122e740e61b1529a5dce142d0b6bd
João Paulo Fricks <jpfricks@gmail.com>
2025-10-16 23:37:35 -0300

    fix: correct capitalization in heading and add margin

--- src/pages/general.tsx
+++ src/pages/general.tsx
@@ -2,10 +2,10 @@ import { Box, Divider, Flex, Heading } from "@chakra-ui/react";
 
 export default function GeneralRegistrationPage() {
   return (
-    <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+    <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]} mb={8}>
       <Flex mb="8" direction="column">
         <Heading size="lg" fontWeight="normal">
-          Cadastro Geral
+          Cadastro geral
         </Heading>
         <Divider my="6" borderColor="gray.700" />
       </Flex>
