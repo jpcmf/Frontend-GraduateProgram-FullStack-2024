71e9a658ee36ee09fcf4b4e1f82e589295c1d947
João Paulo Fricks <jpfricks@gmail.com>
2025-10-23 23:26:55 -0300

    fix: add margin-bottom to Box component in SpotsPage for improved layout

--- src/pages/spots/index.tsx
+++ src/pages/spots/index.tsx
@@ -2,7 +2,7 @@ import { Box, Divider, Flex, Heading } from "@chakra-ui/react";
 
 export default function SpotsPage() {
   return (
-    <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+    <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]} mb={8}>
       <Flex mb="8" direction="column">
         <Heading size="lg" fontWeight="normal">
           Spots
