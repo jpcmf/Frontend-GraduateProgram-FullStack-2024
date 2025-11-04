4efc01485219ab4c123ed6e4fcecefd71d3cdecb
João Paulo Fricks <jpfricks@gmail.com>
2025-11-03 22:06:57 -0300

    refactor: move categories to constants for better maintainability

--- src/features/user/edit/index.tsx
+++ src/features/user/edit/index.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
 import { Toast } from "@/components/Toast";
+import { CATEGORIES } from "@/const/categories";
 import { AuthContext } from "@/contexts/AuthContext";
 import { Input } from "@/shared/components/Form/Input";
 import { Select } from "@/shared/components/Form/Select";
@@ -36,20 +37,6 @@ const UserEditFormSchema = z.object({
 });
 
 type UserEditFormSchema = z.infer<typeof UserEditFormSchema>;
-
-const CATEGORIES = [
-  { id: 1, name: "Iniciante", value: "iniciante" },
-  { id: 2, name: "Amador", value: "amador" },
-  { id: 3, name: "Profissional", value: "profissional" },
-  { id: 4, name: "Pro Master", value: "pro-master" },
-  { id: 5, name: "Pro Legend", value: "pro-legend" },
-  { id: 6, name: "Master", value: "master" },
-  { id: 7, name: "Grand Master", value: "grand-master" },
-  { id: 8, name: "Grand Legend", value: "grand-legend" },
-  { id: 9, name: "Vintage", value: "vintage" },
-  { id: 10, name: "Open", value: "open" },
-  { id: 11, name: "Paraskatista", value: "paraskatista" }
-];
 
 export function UserEdit() {
   const router = useRouter();
