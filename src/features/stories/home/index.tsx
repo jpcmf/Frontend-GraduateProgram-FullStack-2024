d0d20fdedac460831c218902329bf618d09c7b78
João Paulo Fricks <jpfricks@gmail.com>
2025-11-05 15:43:36 -0300

    refactor: update Heading size in StoriesHome component for improved visibility

--- src/features/stories/home/index.tsx
+++ src/features/stories/home/index.tsx
@@ -7,7 +7,7 @@ export function StoriesHome() {
     <>
       <Box mb={6}>
         <Flex direction="row" alignItems="center" position="relative">
-          <Heading size="md" fontWeight="semibold" bg="gray.900" py={0} pr={4}>
+          <Heading size="lg" fontWeight="semibold" bg="gray.900" py={0} pr={4}>
             Online agora
           </Heading>
           <Divider my="0" borderColor="gray.700" position="absolute" left={0} right={0} zIndex={-1} />
