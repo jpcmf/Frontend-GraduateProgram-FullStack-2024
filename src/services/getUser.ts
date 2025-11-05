15e8be8d0e26cf5748971a4bcc3f67d827033cb9
João Paulo Fricks <jpfricks@gmail.com>
2025-11-05 14:58:31 -0300

    fix: enhance getUser function with error handling and type definition

--- src/services/getUser.ts
+++ src/services/getUser.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+import type { UserBasics } from '@/types/usersBasics.type';
 import { API } from "@/utils/constant";
 
-export async function getUser(id: string) {
-  const res = await axios.get(`${API}/api/users/${id}?populate=avatar,address,category`);
-
-  return res.data;
+export async function getUser(id: string): Promise<UserBasics> {
+  try {
+    const res = await axios.get(`${API}/api/users/${id}?populate=avatar,address,category`);
+    return res.data;
+  } catch (error) {
+    throw new Error(`Failed to fetch user data: ${error}`);
+  }
 }
