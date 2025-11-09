0fa841e91fe302cbbc36032ed8246086cb267259
João Paulo Fricks <jpfricks@gmail.com>
2025-11-09 18:35:17 -0300

    feat: add TitleSection component

---
+++ src/components/TitleSection/index.tsx
@@ -0,0 +1,16 @@
+import { Box, Divider, Flex, Heading, useColorModeValue } from '@chakra-ui/react';
+
+export function TitleSection({ title }: { title: string }) {
+  const titleBgColor = useColorModeValue("white", "gray.900");
+
+  return (
+    <Box mb={6}>
+      <Flex direction="row" alignItems="center" position="relative">
+        <Heading size="lg" bg={titleBgColor} py={0} pr={4}>
+          {title}
+        </Heading>
+        <Divider my="0" borderColor="gray.700" position="absolute" left={0} right={0} zIndex={-1} />
+      </Flex>
+    </Box>
+  );
+}
