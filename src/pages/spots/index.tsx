3f8105969ed5d940fed57219fb5027f7a7ede13d
João Paulo Fricks <jpfricks@gmail.com>
2025-11-08 23:32:58 -0300

    refactor: enhance with color mode support

--- src/pages/spots/index.tsx
+++ src/pages/spots/index.tsx
@@ -1,17 +1,19 @@
-import { Box, Divider, Flex, Grid, Heading } from "@chakra-ui/react";
+import { Box, Divider, Flex, Grid, Heading, useColorModeValue } from "@chakra-ui/react";
 
 export default function SpotsPage() {
+  const bgColor = useColorModeValue("blackAlpha.100", "gray.800");
+  const titleBgColor = useColorModeValue("white", "gray.900");
   return (
     <>
       <Box mb={6}>
         <Flex direction="row" alignItems="center" position="relative">
-          <Heading size="lg" bg="gray.900" py={0} pr={4}>
+          <Heading size="lg" bg={titleBgColor} py={0} pr={4}>
             Spots
           </Heading>
           <Divider my="0" borderColor="gray.700" position="absolute" left={0} right={0} zIndex={-1} />
         </Flex>
       </Box>
-      <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]} mb={8}>
+      <Box flex="1" borderRadius={8} bg={bgColor} p={["6", "8"]} mb={8}>
         <Flex mb="8" direction="column">
           <Grid templateColumns={{ base: "1fr", sm: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }} gap={6}>
             <Box>Image 1</Box>
