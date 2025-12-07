5d996bcc856e3dea33b865d9c95c6aa4b3391128
João Paulo Fricks <jpfricks@gmail.com>
2025-12-06 22:44:17 -0300

    feat: add getStories and getStoriesByUserId functions for story retrieval

---
+++ src/services/getStories.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+import { API } from "@/utils/constant";
+
+export async function getStories() {
+  const res = await axios.get(
+    `${API}/api/stories?populate[author][fields][0]=username&populate[author][fields][1]=avatar`
+  );
+  console.log("res...", res);
+
+  return res.data;
+}
+
+export async function getStoriesByUserId(userId: string) {
+  const res = await axios.get(
+    `${API}/api/stories?filters[author][id][$eq]=${userId}&populate[author][fields][0]=username&populate[author][fields][1]=avatar`
+  );
+
+  return res.data;
+}
