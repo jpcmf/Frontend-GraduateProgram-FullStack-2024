d3c9bec7cf5eae11bea68380067455fbe2678736
João Paulo Fricks <jpfricks@gmail.com>
2025-12-10 00:03:59 -0300

    fix: change endPage variable to const

--- src/features/skatistas/index.tsx
+++ src/features/skatistas/index.tsx
@@ -56,7 +56,7 @@ export function Skatistas({
     const pages = [];
     const maxVisiblePages = 5;
     let startPage = Math.max(0, currentPage - Math.floor(maxVisiblePages / 2));
-    let endPage = Math.min(totalPages - 1, startPage + maxVisiblePages - 1);
+    const endPage = Math.min(totalPages - 1, startPage + maxVisiblePages - 1);
 
     if (endPage - startPage < maxVisiblePages - 1) {
       startPage = Math.max(0, endPage - maxVisiblePages + 1);
