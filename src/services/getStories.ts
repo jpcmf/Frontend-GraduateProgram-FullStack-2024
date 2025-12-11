e3af6962bd3ed742bcfc9c4e28eea05e06828703
João Paulo Fricks <jpfricks@gmail.com>
2025-12-10 23:26:29 -0300

    fix: update return type

--- src/services/getStories.ts
+++ src/services/getStories.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+import type { StoriesResponse } from '@/types/stories';
 import { API } from "@/utils/constant";
 
-export async function getStories(): Promise<any> {
+export async function getStories(): Promise<StoriesResponse> {
   try {
     const res = await axios.get(
       `${API}/api/stories?populate[author][fields][0]=username&populate[author][fields][1]=avatar`
@@ -14,7 +15,7 @@ export async function getStories(): Promise<any> {
   }
 }
 
-export async function getStoriesByUserId(userId: string): Promise<any> {
+export async function getStoriesByUserId(userId: string): Promise<StoriesResponse> {
   try {
     const res = await axios.get(
       `${API}/api/stories?filters[author][id][$eq]=${userId}&populate[author][fields][0]=username&populate[author][fields][1]=avatar`
