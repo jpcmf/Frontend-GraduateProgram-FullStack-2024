9dff9f6e1f10609ff445b13ccf3fb31c0f1e4ef3
João Paulo Fricks <jpfricks@gmail.com>
2025-12-10 23:19:07 -0300

    fix: add error handling to getStories and getStoriesByUserId functions

--- src/services/getStories.ts
+++ src/services/getStories.ts
@@ -2,18 +2,26 @@ import axios from "axios";
 
 import { API } from "@/utils/constant";
 
-export async function getStories() {
-  const res = await axios.get(
-    `${API}/api/stories?populate[author][fields][0]=username&populate[author][fields][1]=avatar`
-  );
-
-  return res.data;
+export async function getStories(): Promise<any> {
+  try {
+    const res = await axios.get(
+      `${API}/api/stories?populate[author][fields][0]=username&populate[author][fields][1]=avatar`
+    );
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching stories:", error);
+    throw error;  
+  }
 }
 
-export async function getStoriesByUserId(userId: string) {
-  const res = await axios.get(
-    `${API}/api/stories?filters[author][id][$eq]=${userId}&populate[author][fields][0]=username&populate[author][fields][1]=avatar`
-  );
-
-  return res.data;
+export async function getStoriesByUserId(userId: string): Promise<any> {
+  try {
+    const res = await axios.get(
+      `${API}/api/stories?filters[author][id][$eq]=${userId}&populate[author][fields][0]=username&populate[author][fields][1]=avatar`
+    );
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching stories by user ID:", error);
+    throw error;
+  }
 }
