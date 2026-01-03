e1ca6441529497a46aacaac81f2d4ca37ae0d545
João Paulo Fricks <jpfricks@gmail.com>
2026-01-02 22:53:53 -0300

    feat: update DashboardPage with localized title and clean up commented code

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,17 +1,22 @@
 import { useContext } from "react";
 
 import { Box } from "@chakra-ui/react";
+import { parseCookies } from "nookies";
 
 import { TitleSection } from "@/components/TitleSection";
-// import { parseCookies } from "nookies";
 import { AuthContext } from "@/contexts/AuthContext";
 import { Dashboard } from "@/features/dashboard";
 
 export default function DashboardPage() {
   const { user } = useContext(AuthContext);
+
+  if (!user) {
+    return <Box>Loading...</Box>;
+  }
+
   return (
     <>
-      <TitleSection title="Dashboard" />
+      <TitleSection title="Painel do criador" />
       <Box flex="1" borderRadius={8} mb={8}>
         <Dashboard user={user} />
       </Box>
@@ -19,18 +24,18 @@ export default function DashboardPage() {
   );
 }
 
-// export const getServerSideProps = async (ctx: any) => {
-//   const { ["auth.token"]: token } = parseCookies(ctx);
+export const getServerSideProps = async (ctx: any) => {
+  const { ["auth.token"]: token } = parseCookies(ctx);
 
-//   if (!token) {
-//     return {
-//       redirect: {
-//         destination: "/",
-//         permanent: false
-//       }
-//     };
-//   }
-//   return {
-//     props: {}
-//   };
-// };
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false
+      }
+    };
+  }
+  return {
+    props: {}
+  };
+};
