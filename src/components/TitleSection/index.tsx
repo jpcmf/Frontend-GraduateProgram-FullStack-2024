3d0721f000cfcb5fe40862f93c49cefad22ea7dc
João Paulo Fricks <jpfricks@gmail.com>
2026-02-25 21:52:20 -0300

    fix: enhance TitleSection component with fontWeight prop and clean up code

--- src/components/TitleSection/index.tsx
+++ src/components/TitleSection/index.tsx
@@ -1,15 +1,20 @@
-import { Box, Divider, Flex, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, Heading, useColorModeValue } from "@chakra-ui/react";
 
-export function TitleSection({ title, size = "lg" }: { title: string; size?: string }) {
+interface TitleSectionProps {
+  title: string;
+  size?: string;
+  fontWeight?: string;
+}
+
+export function TitleSection({ title, size = "lg", fontWeight = "semibold" }: TitleSectionProps) {
   const titleBgColor = useColorModeValue("white", "gray.900");
 
   return (
     <Box mb={6}>
-      <Flex direction="row" alignItems="center" position="relative">
-        <Heading size={size} bg={titleBgColor} py={0} pr={4}>
+      <Flex direction="row" alignItems="center">
+        <Heading size={size} fontWeight={fontWeight} bg={titleBgColor} py={0} pr={4}>
           {title}
         </Heading>
-        <Divider my="0" borderColor="gray.700" position="absolute" left={0} right={0} zIndex={-1} />
       </Flex>
     </Box>
   );
