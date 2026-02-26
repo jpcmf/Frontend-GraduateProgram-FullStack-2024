0a030c72de51d5ac23410797eee9d6965c2e95e7
João Paulo Fricks <jpfricks@gmail.com>
2026-02-25 21:54:57 -0300

    fix: simplify StoriesHome layout by replacing Heading and Divider with TitleSection component

--- src/features/stories/home/index.tsx
+++ src/features/stories/home/index.tsx
@@ -1,10 +1,10 @@
-import { Box, Divider, Flex, Heading, Spinner, Text, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, Spinner, Text } from "@chakra-ui/react";
 
 import { StoriesSwiper } from "@/components/StoriesSwiper";
+import { TitleSection } from "@/components/TitleSection";
 import { useStories } from "@/hooks/useStories";
 
 export function StoriesHome() {
-  const titleBgColor = useColorModeValue("white", "gray.900");
   const { data, isLoading, isError } = useStories();
 
   const stories =
@@ -35,12 +35,7 @@ export function StoriesHome() {
   return (
     <>
       <Box mb={6}>
-        <Flex direction="row" alignItems="center" position="relative">
-          <Heading size="lg" fontWeight="semibold" bg={titleBgColor} py={0} pr={4}>
-            Online agora
-          </Heading>
-          <Divider my="0" borderColor="gray.700" position="absolute" left={0} right={0} zIndex={-1} />
-        </Flex>
+        <TitleSection title="Online agora" />
       </Box>
       <StoriesSwiper stories={stories} />
     </>
