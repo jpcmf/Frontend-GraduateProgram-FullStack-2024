943742f6f931ad13c64a3c67ecd0596fccc27f7c
João Paulo Fricks <jpfricks@gmail.com>
2026-04-12 17:26:40 -0300

    fix(spots): standardize import statement quotes in SpotsPage component

--- src/pages/spots/index.tsx
+++ src/pages/spots/index.tsx
@@ -2,7 +2,7 @@ import NextLink from "next/link";
 
 import { Box, Button, Flex, Grid, Spinner, Text } from "@chakra-ui/react";
 
-import { TitleSection } from '@/components/TitleSection';
+import { TitleSection } from "@/components/TitleSection";
 import { SpotCard } from "@/features/spots/SpotCard";
 import { useAuth } from "@/hooks/useAuth";
 import { useSpots } from "@/hooks/useSpots";
