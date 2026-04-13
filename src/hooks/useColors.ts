d38776d1e37655cb7dd8647c4c605c2d1e25fca8
João Paulo Fricks <jpfricks@gmail.com>
2026-04-13 18:21:05 -0300

    feat(useColors): update card background color for consistency across themes

--- src/hooks/useColors.ts
+++ src/hooks/useColors.ts
@@ -3,7 +3,7 @@ import { useColorModeValue } from "@chakra-ui/react";
 export function useColors() {
   return {
     bgColor: useColorModeValue("blackAlpha.100", "gray.800"),
-    cardBg: useColorModeValue("white", "gray.700"),
+    cardBg: useColorModeValue("blackAlpha.100", "gray.800"),
     textSecondary: useColorModeValue("gray.500", "zinc.400"),
     border: useColorModeValue("gray.200", "gray.600"),
     textPrimary: useColorModeValue("gray.900", "whiteAlpha.900")
