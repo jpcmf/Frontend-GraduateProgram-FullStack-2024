a302448b9f3de590ebc4ba4e4a238257657c3a1d
João Paulo Fricks <jpfricks@gmail.com>
2026-04-13 17:55:25 -0300

    feat(newSpotPage): refactor layout and integrate TitleSection for improved UI

--- src/pages/spots/new.tsx
+++ src/pages/spots/new.tsx
@@ -1,14 +1,18 @@
+import Head from "next/dist/shared/lib/head";
 import { useRouter } from "next/router";
 
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
+import { TitleSection } from "@/components/TitleSection";
 import { SpotForm } from "@/features/spots/SpotForm";
+import { useColors } from "@/hooks/useColors";
 import { useCreateSpot } from "@/hooks/useCreateSpot";
 import type { CreateSpotPayload } from "@/types/spots";
 
 export default function NewSpotPage() {
   const router = useRouter();
   const { mutateAsync: createSpot, isPending, error } = useCreateSpot();
+  const { bgColor } = useColors();
 
   async function handleSubmit(payload: CreateSpotPayload) {
     const res = await createSpot(payload);
@@ -16,16 +20,21 @@ export default function NewSpotPage() {
   }
 
   return (
-    <Box w={"100%"}>
-      <Heading size="lg" mb={8}>
-        Novo Spot
-      </Heading>
-      {error && (
-        <Text color="red.400" mb={4}>
-          Erro ao criar spot. Tente novamente.
-        </Text>
-      )}
-      <SpotForm onSubmit={handleSubmit} isSubmitting={isPending} submitLabel="Criar Spot" />
-    </Box>
+    <>
+      <Head>
+        <title>Novo Spot - SkateHub</title>
+      </Head>
+      <TitleSection title="Novo Spot" />
+      <Flex alignItems="center" flexDirection="column" height="100%" justifyContent="start" mb={8} width="100%">
+        <Box w={"100%"} p={8} borderRadius={8} bg={bgColor}>
+          {error && (
+            <Text color="red.400" mb={4}>
+              Erro ao criar spot. Tente novamente.
+            </Text>
+          )}
+          <SpotForm onSubmit={handleSubmit} isSubmitting={isPending} submitLabel="Criar Spot" />
+        </Box>
+      </Flex>
+    </>
   );
 }
