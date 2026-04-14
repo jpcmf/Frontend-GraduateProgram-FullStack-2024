4498c9cd51e150a718ad2af6bdfd4fbc1291287c
João Paulo Fricks <jpfricks@gmail.com>
2026-04-14 19:17:59 -0300

    feat: add logoFillColor and bgColorNoOpacity to useColors hook

--- src/hooks/useColors.ts
+++ src/hooks/useColors.ts
@@ -2,7 +2,9 @@ import { useColorModeValue } from "@chakra-ui/react";
 
 export function useColors() {
   return {
+    logoFillColor: useColorModeValue("#323238", "#ededed"),
     bgColor: useColorModeValue("blackAlpha.100", "gray.800"),
+    bgColorNoOpacity: useColorModeValue("#f0f0f0", "gray.800"),
     borderColor: useColorModeValue("blackAlpha.50", "whiteAlpha.50"),
     cardBg: useColorModeValue("blackAlpha.100", "gray.800"),
     textSecondary: useColorModeValue("gray.500", "zinc.400"),
