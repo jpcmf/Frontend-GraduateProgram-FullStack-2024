8d36444c83098c8675e03e97f410475a27ee86a1
João Paulo Fricks <jpfricks@gmail.com>
2026-04-14 12:12:52 -0300

    fix: add missing borderColor property to useColors hook

--- src/hooks/useColors.ts
+++ src/hooks/useColors.ts
@@ -3,6 +3,7 @@ import { useColorModeValue } from "@chakra-ui/react";
 export function useColors() {
   return {
     bgColor: useColorModeValue("blackAlpha.100", "gray.800"),
+    borderColor: useColorModeValue("blackAlpha.50", "whiteAlpha.50"),
     cardBg: useColorModeValue("blackAlpha.100", "gray.800"),
     textSecondary: useColorModeValue("gray.500", "zinc.400"),
     border: useColorModeValue("gray.200", "gray.600"),
