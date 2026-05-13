55638e54fe3b6039486e6ba13b89eed315bf82c0
João Paulo Fricks <jpfricks@gmail.com>
2026-05-13 20:08:55 -0300

    task(3-1): create src/shared/ui/ directory structure and barrel exports

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,21 @@
+// Form components
+export * from "./form";
+
+// Layout components
+export * from "./layout";
+
+// UI components
+export { ActiveLink } from "./ActiveLink";
+export { AvatarEditProfile } from "./AvatarEditProfile";
+export { CardUser } from "./CardUser";
+export { ErrorBoundary } from "./ErrorBoundary";
+export { HeaderProfile } from "./HeaderProfile";
+export { LogoSkateHub } from "./LogoSkateHub";
+export { MapBox } from "./Map";
+export { Pagination } from "./Pagination";
+export { QueryProvider } from "./QueryProvider";
+export { ReusableModal } from "./ReusableModal";
+export { TitleSection } from "./TitleSection";
+export { Toast } from "./Toast";
+export { StoriesSwiper } from "./StoriesSwiper";
+export { ImproveTextButton } from "./ImproveTextButton";
