d5019633b446eb1652877745ba8dafa434209074
João Paulo Fricks <jpfricks@gmail.com>
2026-05-20 22:26:33 -0300

    fix: reorder spot feature exports

--- src/features/spots/index.ts
+++ src/features/spots/index.ts
@@ -1,21 +1,18 @@
+// Components
+export { SpotCard } from "./components/SpotCard";
+export { SpotDetail } from "./components/SpotDetail";
+export { SpotForm } from "./components/SpotForm";
 // Hooks
 export { useCreateSpot } from "./hooks/useCreateSpot";
 export { useDeleteSpot } from "./hooks/useDeleteSpot";
 export { useSpot } from "./hooks/useSpot";
 export { useSpots } from "./hooks/useSpots";
 export { useUpdateSpot } from "./hooks/useUpdateSpot";
-
 // Services
 export { createSpot } from "./services/createSpot";
 export { deleteSpot } from "./services/deleteSpot";
 export { getSpotById } from "./services/getSpotById";
 export { getSpots } from "./services/getSpots";
 export { updateSpot } from "./services/updateSpot";
-
 // Types
 export type * from "./types/spots";
-
-// Components
-export { SpotCard } from "./components/SpotCard";
-export { SpotDetail } from "./components/SpotDetail";
-export { SpotForm } from "./components/SpotForm";
