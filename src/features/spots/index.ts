7a6d8bebacc1148a4c67277077d9d73198875206
João Paulo Fricks <jpfricks@gmail.com>
2026-08-13 17:41:39 -0300

    refactor(seo): remove dead hooks and client service after SSR conversion

--- src/features/spots/index.ts
+++ src/features/spots/index.ts
@@ -6,8 +6,6 @@ export { SpotsCreateButton } from "./components/SpotsCreateButton";
 // Hooks
 export { useCreateSpot } from "./hooks/useCreateSpot";
 export { useDeleteSpot } from "./hooks/useDeleteSpot";
-export { useSpot } from "./hooks/useSpot";
-export { useSpots } from "./hooks/useSpots";
 export { useUpdateSpot } from "./hooks/useUpdateSpot";
 // Server Services
 export { getSpotServer } from "./services/getSpot.server";
@@ -16,7 +14,6 @@ export { getSpotsServer } from "./services/getSpots.server";
 export { createSpot } from "./services/createSpot";
 export { deleteSpot } from "./services/deleteSpot";
 export { getSpotById } from "./services/getSpotById";
-export { getSpots } from "./services/getSpots";
 export { updateSpot } from "./services/updateSpot";
 // Types
 export type * from "./types/spots";
